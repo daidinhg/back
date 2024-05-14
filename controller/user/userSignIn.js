b38ab66233f7ff3d61e7494d4d6b481846778421
daidinhg <kecodoc0909@gmail.com>
2024-05-14 19:43:20 +0700

    a

--- controller/user/userSignIn.js
+++ controller/user/userSignIn.js
@@ -31,8 +31,9 @@ async function userSignInController(req,res){
         const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
 
         const tokenOption = {
-            httpOnly : true,
-            secure : true
+            httpOnly: true,
+                secure: process.env.NODE_ENV === 'production', // Secure chỉ khi ở production
+                sameSite: 'None' 
         }
 
         res.cookie("token",token,tokenOption).status(200).json({
