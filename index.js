0f34b2280913c6ad769a27ba3a71679a09037e20
daidinhg <kecodoc0909@gmail.com>
2024-05-14 19:23:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express()
 app.use(cors({
     origin : process.env.FRONTEND_URL,
     credentials : true
+    ,methods:['GET','POST','PUT','DELETE']
 }))
 app.use(express.json())
 app.use(cookieParser())
