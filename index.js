f783e560d2745962a059b40756d0a9539fc385bf
daidinhg <kecodoc0909@gmail.com>
2024-05-14 19:57:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : 'https://front-three-jade.vercel.app/',
     credentials : true
     ,methods:['GET','POST','PUT','DELETE']
 }))
